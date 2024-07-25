565d0c94610dc02ca3836b6c56a8a95cc6e547a4
MattFr <matthieu_franck@orange.fr>
2024-07-25 18:51:42 +0200

    adding searchbar

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css',
+})
+export class SearchBarComponent {
+  @Output() checkEnteredText = new EventEmitter<string>();
+  enteredText = '';
+
+  onSubmit() {
+    console.log('submitted!');
+    this.checkEnteredText.emit(this.enteredText);
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, SearchBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  enteredText = signal<string>('');
+
+  checkInput(data: string) {
+    this.enteredText.set(data);
+    console.log(this.enteredText());
+  }
+}
