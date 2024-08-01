2a39acc98f135dff93b50b98c4eeeceaf1d06590
MattFr <matthieu_franck@orange.fr>
2024-08-01 12:09:17 +0200

    added new value and searchbar data2

--- src/app/pages/stock/stock.component.ts
+++ src/app/pages/stock/stock.component.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, DestroyRef, inject } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { environment } from '../../../environments/environment.development';
 import { StockInfoApiResponse } from '../../models/StockInfo.model';
 import { GraphComponent } from '../../components/graph/graph.component';
+import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-stock',
   standalone: true,
-  imports: [RouterLink, GraphComponent],
+  imports: [RouterLink, GraphComponent, SearchBarComponent],
   templateUrl: './stock.component.html',
   styleUrl: './stock.component.css',
 })
@@ -18,6 +19,8 @@ export class StockComponent {
   private route = inject(ActivatedRoute);
   data: StockInfoApiResponse | null = null;
   pic: string = '';
+
+  enteredText = signal<string>('');
 
   data1: number[] = [];
   data2: number[] = [];
@@ -64,23 +67,28 @@ export class StockComponent {
     } else {
       console.error('Ticker not found in the URL');
     }
+  }
 
-    if (ticker) {
+  submitInput(data: string) {
+    this.enteredText.set(data);
+    console.log(this.enteredText());
+
+    if (data) {
       const subscription = this.httpClient
         .get<{ results: { c: number }[] }>(
-          `https://api.polygon.io/v2/aggs/ticker/MSFT/range/1/month/2023-01-09/2024-02-10?adjusted=true&sort=asc&apiKey=${environment.apiKeyPolygon}`
+          `https://api.polygon.io/v2/aggs/ticker/${data}/range/1/month/2023-01-09/2024-02-10?adjusted=true&sort=asc&apiKey=${environment.apiKeyPolygon}`
         )
         .subscribe({
           next: (resData) => {
-            console.log(resData);
             this.data2 = resData.results.map((result) => result.c);
+            console.log(this.data2);
           },
         });
       this.destroyRef.onDestroy(() => {
         subscription.unsubscribe();
       });
     } else {
-      console.error('Ticker not found in the URL');
+      console.error('Ticker not found in the searchbar');
     }
   }
 }
