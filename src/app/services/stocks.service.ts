99fbc79e7c614a6ee3a261bb03be8b49a20a8266
MattFr <matthieu_franck@orange.fr>
2024-08-21 14:58:02 +0200

    add functionality to switch api keys in services

--- src/app/services/stocks.service.ts
+++ src/app/services/stocks.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
+import { catchError, Observable, throwError } from 'rxjs';
 
 type DateRange = 'oneDay' | 'fiveDays' | 'oneMonth' | 'threeMonths';
 
@@ -9,6 +10,19 @@ type DateRange = 'oneDay' | 'fiveDays' | 'oneMonth' | 'threeMonths';
 })
 export class StocksService {
   private httpClient = inject(HttpClient);
+
+  private apiKeys = [environment.apiKeyPolygon, environment.apiKeyPolygon2];
+  private currentApiKeyIndex = 0;
+
+  private getApiKey(): string {
+    return this.apiKeys[this.currentApiKeyIndex];
+  }
+
+  // switch api key
+  private rotateApiKey() {
+    this.currentApiKeyIndex =
+      (this.currentApiKeyIndex + 1) % this.apiKeys.length;
+  }
 
   constructor() {}
 
@@ -50,15 +64,38 @@ export class StocksService {
     }
   }
 
-  loadStockData(ticker: string, range: DateRange) {
+  loadStockData(
+    ticker: string,
+    range: DateRange,
+    retryCount: number = 0
+  ): Observable<{ results: { c: number; t: number }[]; ticker: string }> {
     const endDate = this.getCurrentDateInUSEast();
     const rangeString = this.getRangeString(range, endDate);
 
-    return this.httpClient.get<{
-      results: { c: number; t: number }[];
-      ticker: string;
-    }>(
-      `https://api.polygon.io/v2/aggs/ticker/${ticker}/${rangeString}?adjusted=true&sort=asc&apiKey=${environment.apiKeyPolygon}`
-    );
+    return this.httpClient
+      .get<{
+        results: { c: number; t: number }[];
+        ticker: string;
+      }>(
+        `https://api.polygon.io/v2/aggs/ticker/${ticker}/${rangeString}?adjusted=true&sort=asc&apiKey=${this.getApiKey()}`
+      )
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 429 && retryCount < this.apiKeys.length) {
+            // Rotate the API key if rate limit is hit
+            this.rotateApiKey();
+            // Retry the request with the new API key
+            return this.loadStockData(ticker, range, retryCount + 1);
+          } else {
+            // Re-throw any other error or stop retrying if all API keys have been tried
+            return throwError(
+              () =>
+                new Error(
+                  `Failed after ${retryCount} retries: ${error.message}`
+                )
+            );
+          }
+        })
+      );
   }
 }
