65eb36b4727252fb179b78e778234587b07999d7
MattFr <matthieu_franck@orange.fr>
2024-09-03 11:29:30 +0200

    change the searchbar

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,26 +1,36 @@
-import {
-  Component,
-  EventEmitter,
-  input,
-  output,
-  Output,
-  signal,
-} from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule,
+    ButtonModule,
+  ],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css',
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent {
-  checkEnteredText = output<string>();
+  @Output() checkEnteredText = new EventEmitter<string>();
   enteredText = signal('');
 
   onSubmit() {
     console.log('submitted!');
     this.checkEnteredText.emit(this.enteredText());
+  }
+
+  onInputChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement | null;
+    if (inputElement) {
+      this.enteredText.set(inputElement.value);
+    }
   }
 }
