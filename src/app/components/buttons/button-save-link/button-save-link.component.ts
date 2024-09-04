63586ac06d55a217459f20a80190daea6dfc97d6
MattFr <matthieu_franck@orange.fr>
2024-09-04 18:34:32 +0200

    deleted unused imports

--- src/app/components/buttons/button-save-link/button-save-link.component.ts
+++ src/app/components/buttons/button-save-link/button-save-link.component.ts
@@ -1,20 +1,11 @@
 import { Component, input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { ButtonGroupModule } from 'primeng/buttongroup';
-import { ToggleButtonModule } from 'primeng/togglebutton';
 
 @Component({
   selector: 'app-button-save-link',
   standalone: true,
-  imports: [
-    ButtonModule,
-    RouterModule,
-    ToggleButtonModule,
-    FormsModule,
-    ButtonGroupModule,
-  ],
+  imports: [ButtonModule, RouterModule],
   templateUrl: './button-save-link.component.html',
   styleUrl: './button-save-link.component.css',
 })
