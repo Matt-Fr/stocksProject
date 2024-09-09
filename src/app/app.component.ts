7b27b9c00c7c9d4c23cad89e4fde475b570a2e79
MattFr <matthieu_franck@orange.fr>
2024-09-09 13:30:26 +0200

    add ripple effect btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  private primengConfig = inject(PrimeNGConfig);
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+}
