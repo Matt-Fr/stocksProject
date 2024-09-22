2ee650b11996698d687ece866caabe0adecf4beb
MattFr <matthieu_franck@orange.fr>
2024-09-22 20:31:12 +0200

    test with empty string

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiKeyPolygon: _NGX_ENV_['NG_APP_apiKeyPolygon'],
-  apiKeyPolygon2: _NGX_ENV_['NG_APP_apiKeyPolygon2'],
-  apiKeyNews: _NGX_ENV_['NG_APP_apiKeyNews'],
-  apiKeyTickerNews: _NGX_ENV_['NG_APP_apiKeyTickerNews'],
-  apiKeyBrandfetch: _NGX_ENV_['NG_APP_apiKeyBrandfetch'],
+  apiKeyPolygon: _NGX_ENV_['NG_APP_apiKeyPolygon'] || '',
+  apiKeyPolygon2: _NGX_ENV_['NG_APP_apiKeyPolygon2'] || '',
+  apiKeyNews: _NGX_ENV_['NG_APP_apiKeyNews'] || '',
+  apiKeyTickerNews: _NGX_ENV_['NG_APP_apiKeyTickerNews'] || '',
+  apiKeyBrandfetch: _NGX_ENV_['NG_APP_apiKeyBrandfetch'] || '',
 };
