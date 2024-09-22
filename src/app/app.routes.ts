d95a6aa74141183d9ad85897b28fc2e543a3418a
MattFr <matthieu_franck@orange.fr>
2024-09-22 19:59:36 +0200

    add redirect and pathmatch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { CalculatorComponent } from './pages/calculator/calculator.component';
 import { StockComponent } from './pages/stock/stock.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'calculator', component: CalculatorComponent },
   { path: ':ticker', component: StockComponent },
 ];
