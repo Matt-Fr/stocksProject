b3e8644b4ed0d02b37e43c9ba6f5b9b8f394f7f3
MattFr <matthieu_franck@orange.fr>
2024-10-28 10:33:56 +0100

    add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CalculatorComponent } from './pages/calculator/calculator.component';
 import { StockComponent } from './pages/stock/stock.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'calculator', component: CalculatorComponent },
   { path: 'ticker/:ticker', component: StockComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
